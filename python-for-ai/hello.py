cb78f02064d6c2fa7b9a1aad602f55bebecb713f
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2026-01-16 22:33:53 +0100

    installed requests package and called the get method

--- python-for-ai/hello.py
+++ python-for-ai/hello.py
@@ -1,1 +1,8 @@
 print("Hello, World!")
+print("I am learning Python for AI.")
+
+import requests
+
+# Download a webpage
+response = requests.get("https://api.github.com")
+print(response.status_code)
